da93f339f070b38c0724cf974797b3ea52784e04
Sparrow1488 <ilyaokubev@gmail.com>
2021-12-21 09:20:44 +0300

    не зря выбрал OpenTK, исчерпывающее кол-во мануалов и просто логично работающая либа, одобряю

--- src/OpenGL.Intro/OpenGl.SapperTK/Windows/Game.cs
+++ src/OpenGL.Intro/OpenGl.SapperTK/Windows/Game.cs
@@ -7,10 +7,93 @@ namespace OpenGl.SapperTK.Windows
 {
     internal class Game : GameWindow
     {
+        private int _verticesBuffer;
+        private int _vertexArray;
+
+        private int _vertexShader;
+        private int _fragmentShader;
+
+        private int _shaderProgram;
+
         public Game() : 
             base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
-            CenterWindow(new Vector2i(500, 500));
+            CenterWindow(new Vector2i(720, 500));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad()
+        {
+            var vertices = new float[]{
+                -0.5f, -0.5f, 0f,
+                0f, 0.5f, 0f,
+                0.5f, -0.5f, 0f,
+            };
+            _verticesBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _verticesBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            _vertexArray = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _verticesBuffer);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindVertexArray(0);
+
+            string vertexShaderSource = "#version 330 core\n" +
+            "layout (location = 0) in vec3 aPos;\n" + 
+            "void main()\n" +
+            "{\n" +
+            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
+            "}\0";
+            _vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(_vertexShader, vertexShaderSource);
+            GL.CompileShader(_vertexShader);
+
+            string fragmentShaderSource = "#version 330 core\n" +
+            "out vec4 FragColor;\n" +
+            "void main()\n" +
+            "{\n" +
+            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" +
+            "}";
+            _fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(_fragmentShader, fragmentShaderSource);
+            GL.CompileShader(_fragmentShader);
+
+            _shaderProgram = GL.CreateProgram();
+            GL.AttachShader(_shaderProgram, _vertexShader);
+            GL.AttachShader(_shaderProgram, _fragmentShader);
+            GL.LinkProgram(_shaderProgram);
+
+            GL.DetachShader(_shaderProgram, _vertexShader);
+            GL.DetachShader(_shaderProgram, _fragmentShader);
+
+            GL.DeleteShader(_vertexShader);
+            GL.DeleteShader(_fragmentShader);
+
+            GL.UseProgram(_shaderProgram);
+
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            // выгружаем все ресурсы
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffers(0, ref _verticesBuffer);
+
+            GL.UseProgram(_shaderProgram);
+            GL.DeleteProgram(_shaderProgram);
+
+            base.OnUnload();
         }
 
         protected override void OnRenderFrame(FrameEventArgs args)
@@ -18,13 +101,13 @@ namespace OpenGl.SapperTK.Windows
             GL.ClearColor(new Color4(0.3f, 0.5f, 0.4f, 1f));
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            GL.UseProgram(_shaderProgram);
+            GL.BindVertexArray(_vertexArray);
+            GL.PointSize(5f);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
             Context.SwapBuffers();
             base.OnRenderFrame(args);
-        }
-
-        protected override void OnUpdateFrame(FrameEventArgs args)
-        {
-            base.OnUpdateFrame(args);
         }
     }
 }
